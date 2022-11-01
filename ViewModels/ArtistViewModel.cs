12104f1cc6ba795a17cccdf2bb4843dccc979e84
Mel <emanm@mail.nmc.edu>
2022-11-01 02:40:28 -0400

    Add project files.

---
+++ ViewModels/ArtistViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicStore.Models;
+
+namespace MusicStore.ViewModels
+{
+    //action method in the album controller is going to process this and send the data to a view
+    public class ArtistViewModel
+    {
+        public List<Album> Album { get; set; }
+        public List<Artist> Artist { get; set; }
+    }
+}
